e851371878623c046cb31502d9855d1b49da3ae5
kilisei <seitzkili+Github@gmail.com>
2025-01-22 17:14:18 +0100

    setup

--- app/layouts/company.vue
+++ app/layouts/company.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="bg-blue-500 p-4 border">
+  <div class="p-4 ring-black ring grid grid-cols-[auto_1fr]">
     <div class="p-4 border">
       <slot name="sidebar"/>
     </div>
     <div class="border p-4">
-      Company Layout
+      <h1 class="text-3xl mb-16">
+        Company Layout
+      </h1>
       <slot/>
     </div>
   </div>
